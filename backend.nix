9909d97b490ebbfd5a279245cd94c606885ea2df
Jeremiah S <owner@arouzing.xyz>
2023-05-21 23:36:05 -0400

    adjusting backend

---
+++ backend.nix
@@ -0,0 +1,24 @@
+{ ... }:
+{
+  variable = {
+    backend_s3_region = {
+      type = "string";
+      description = "region";
+    };
+    backend_s3_bucket = {
+      type = "string";
+      description = "This is the bucket for storage of state"
+    };
+    backend_s3_key = {
+      type = "string";
+      description = "the key used for bucket";
+    };
+  };
+  backend = {
+    s3 = {
+      region = "\${ var.backend_s3_region }";
+      bucket = "\${ var.backend_s3_bucket }";
+      key = "\${ var.backend_s3_key }";
+    };
+  };
+}
