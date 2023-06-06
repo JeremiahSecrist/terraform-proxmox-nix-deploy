92ef8549c9f8ae712e74ac5c0cb88653d0b1a335
Jermeiah S <owner@arouzing.xyz>
2023-06-06 14:53:54 -0400

    Add postgress as the backend

--- backend.nix
+++ backend.nix
@@ -1,15 +1,7 @@
 { ... }:
 {
-  variable = {
-    backend_pg_url = {
-      type = "string";
-      description = "full path with credentials to pg server";
-    };
-  };
-
   terraform = {
     backend.pg = {
-      conn_str = "\${ var.backend_pg_url }";
     };
   };
 }
