2034d9331463cbe2ae14e17628a8d8a43baeef7f
Jermeiah S <owner@arouzing.xyz>
2023-06-08 17:11:41 -0400

    make disk persist properly

--- singleserver.nix
+++ singleserver.nix
@@ -31,6 +31,27 @@ resource.proxmox_vm_qemu.your-vm = {
         bridge = "vmbr0";
         model  = "virtio";
     };
+    
+    disk = {
+        backup       = 0;
+        cache        = "none";
+        file         = "vm-101-disk-0";
+        format       = "raw";
+        iothread     = 0;
+        mbps         = 0;
+        mbps_rd      = 0;
+        mbps_rd_max  = 0;
+        mbps_wr      = 0;
+        mbps_wr_max  = 0;
+        replicate    = 0;
+        size         = "2456M";
+        slot         = 0;
+        ssd          = 0;
+        storage      = "local-lvm";
+        # storage_type = "lvmthin";
+        type         = "virtio";
+        volume       = "local-lvm:vm-101-disk-0";
+    };
 
     # VM Cloud-Init Settings
     os_type = "cloud-init";
