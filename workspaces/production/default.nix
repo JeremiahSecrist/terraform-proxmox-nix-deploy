d9e353ee0d7b05d7cd8f9db58b2f7bc699970342
Jeremiah S <owner@arouzing.xyz>
2023-06-16 15:49:05 -0400

    bring disk back

--- workspaces/production/default.nix
+++ workspaces/production/default.nix
@@ -32,26 +32,26 @@ resource.proxmox_vm_qemu.newvm = {
         model  = "virtio";
     };
     
-    #disk = {
-    #    backup       = 0;
-    #    cache        = "none";
-    #    file         = "vm-101-disk-0";
-    #    format       = "raw";
-    #    iothread     = 0;
-    #    mbps         = 0;
-    #    mbps_rd      = 0;
-    #    mbps_rd_max  = 0;
-    #    mbps_wr      = 0;
-    #    mbps_wr_max  = 0;
-    #    replicate    = 0;
-    #    size         = "3GB";
-    #    slot         = 0;
-    #    ssd          = 0;
-    #    storage      = "local-lvm";
-    #    # storage_type = "lvmthin";
-    #    type         = "virtio";
-    #    volume       = "local-lvm:vm-101-disk-0";
-    #};
+    disk = {
+        backup       = 0;
+        cache        = "none";
+        file         = "vm-101-disk-0";
+        format       = "raw";
+        iothread     = 0;
+        mbps         = 0;
+        mbps_rd      = 0;
+        mbps_rd_max  = 0;
+        mbps_wr      = 0;
+        mbps_wr_max  = 0;
+        replicate    = 0;
+        size         = "3000M";
+        slot         = 0;
+        ssd          = 0;
+        storage      = "local-lvm";
+        # storage_type = "lvmthin";
+        type         = "virtio";
+        volume       = "local-lvm:vm-101-disk-0";
+    };
 
     # VM Cloud-Init Settings
     os_type = "cloud-init";
